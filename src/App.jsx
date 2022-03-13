c2756498bcdd81763ca9123daea8a6a208c9388b
emidiaz01 <emildsn05@gmail.com>
2022-03-13 19:34:32 -0300

    agrego styled components  h1,div container e img

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import { useState } from 'react'
 import styled from '@emotion/styled'
+import ImagenCriptoPrincipal from './img/imagen-criptos.png'
 
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+`
+const Heading = styled.h1`
+    font-family: 'Lato', sans-serif;
+    color: #fff;
+`
+const Imagen = styled.img`
+    max-width: 400px;
+    width: 80%;
+    margin: 100px auto 0 auto;
+    display: block;
+`
 
 function App() {
-  
-
   return (
-    <h1>Desde App</h1>
+    <Contenedor>
+      <Imagen 
+         src={ImagenCriptoPrincipal}
+         alt='imagen principal criptomonedas'
+      />
+    <Heading>Desde App</Heading>
+    </Contenedor>
+
   )
 }
 
